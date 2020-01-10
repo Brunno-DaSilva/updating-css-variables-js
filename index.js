5503d7fb2f8b4fa51843a033883c260c48082268
LeandroDaSilva Bruno <USDASILVAB@tetrapak.com>
2020-01-10 11:17:43 -0600

    finished the JS logic portion

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const inputs = document.querySelectorAll('.controls input');
 // console.log(inputs);
 
 function handleUpdate (){
-console.log(this.value);
+const suffix = this.dataset.sizing || '';
+
+document.documentElement.style.setProperty(`--${this.name}`, `${this.value}${suffix}`)
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
