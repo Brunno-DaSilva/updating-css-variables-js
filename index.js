2aad65405db6b923cd28c4c4737f098532e62de2
LeandroDaSilva Bruno <USDASILVAB@tetrapak.com>
2020-01-10 10:33:06 -0600

    able to get info display on clg using this.value

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('ciao Mundo');
+
+const inputs = document.querySelectorAll('.controls input');
+
+// // Query Selector returns a NODE_LIST not an Array
+// console.log(inputs);
+
+function handleUpdate (){
+console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
